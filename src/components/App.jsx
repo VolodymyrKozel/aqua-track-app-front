ded3a920c2f328f7a95a5125f521f71ab8248d27
Volodymyr <Volodumurvel@gmail.com>
2024-07-09 17:36:07 +0200

    create project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Children } from 'react';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import HomePage from 'pages/HomePage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <SharedLayout>
+      <HomePage />
+    </SharedLayout>
   );
 };
